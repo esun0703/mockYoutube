8adbce7cf4369884751a999fe960d73ce3504830
Emily Sun <emilysun@Emilys-MacBook-Pro.local>
2017-10-19 14:54:00 -0400

    began to format and insert information for video_list and video_list_item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,9 +2,15 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+	// iterating through array of videos passed in from the props and creating a new array (.map)
+	// returns a component with video
+	// needs a key (which acts like an id);
+	// 
 	const videoItems = props.videos.map(video => {
-		return <VideoListItem video={video}/>
+
+		return <VideoListItem key={video.etag} video={video}/>
 	});
+	// console.log(videoItems);
 
 	return (
 		<ul className="col-md-4 list-group">
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,22 @@
 import React from 'react';
+const VideoListItem = ({video}) => {
+	// {video} will equal to const video = props.video
+	// const video = props.video;
+	// console.log(video);
+	const imgUrl = video.snippet.thumbnails.default.url;
 
-const VideoListItem = (props) => {
-	return <li>Video</li>
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img src={imgUrl} className="media-object"/>
+				</div>
+			</div>
+			<div className="media-body">
+				<div className="media-heading">{video.snippet.title}</div>
+			</div>		
+		</li>
+	);
 };
 
 export default VideoListItem;
